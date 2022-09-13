34f6dec9acf882e0d662c9ed17a449d1dce46b1a
Gr4ffyGw3n <98187030+Gr4ffyGw3n@users.noreply.github.com>
2022-09-13 01:19:20 -0400

    first commit

---
+++ Project Alpha/Assets/Scripts/PlayerBasicMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBasicMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float speed = 10.0f;
+    [SerializeField]
+    public float rotationSpeed = 10.0f;
+    [SerializeField]
+    public float gravity = 10.0f;
+    private bool isGrounded = false;
+    Vector3 targetTranslation;
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float zTranslation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float xTranslation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float yTranslation =- gravity * Time.deltaTime;
+        targetTranslation.x = xTranslation;
+        targetTranslation.y = yTranslation;
+        targetTranslation.z = zTranslation;
+        gameObject.GetComponent<CharacterController>().Move(targetTranslation);
+    }
+
+}
